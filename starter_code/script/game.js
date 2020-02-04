7f28edb3f2d00407c1c5c9f105e89d09574e570e
Ana Flavia Foppa <anaffoppa@gmail.com>
2020-02-04 19:18:42 +0000

    Spell 01, Spell 02 e Spell03 working

--- starter_code/script/game.js
+++ starter_code/script/game.js
@@ -62,25 +62,6 @@ class Game {
 
               }//this.total.spell01>=0 
 
-              /*CHECKCOLLISION SPELL 02*/
-              if(this.total.spell02[0]>=0 && this.total.spell02[1] === true){
-              
-                /*LIFE */
-                this.scoreCharacter-= this.magic02.power;
-                document.getElementById('score-character').style.width = this.scoreCharacter.toString() + '%';
-    
-              }//this.total.spell02>=0
-
-
-              /*CHECKCOLLISION SPELL 03*/
-              if(this.total.spell03[0]>=0 && this.total.spell03[1] === true){
-              
-                /*LIFE */
-                this.scoreCharacter-= this.magic03.power;
-                document.getElementById('score-character').style.width = this.scoreCharacter.toString() + '%';
-      
-              }//this.total.spell03>=0 
-    
               this.scoreController = 0;
             }
           }//this.magic
@@ -90,6 +71,84 @@ class Game {
       }//if(collision)
      
     }//if(this.magic)
+
+    /*SPELL 02**************************************/
+    if(this.magic02){
+      if(this.total.spell02[1] === true){
+        /* Magic Position */
+        const magicX = (this.magic02.positionX + this.magic02.startX)*GRID_SIZE;
+        const magicY = (this.magic02.positionY + this.magic02.startY)*GRID_SIZE;
+    
+
+        /*CHECKCOLLISION*/
+        if(
+          /*UP*/
+          Math.round(characterY) <= Math.round(magicY) + 25 &&
+          Math.round(characterX) + 50 >= Math.round(magicX) &&
+
+          /*BOTTOM*/
+          Math.round(characterY) + 50 > Math.round(magicY) 
+          ){
+        
+          if(this.scoreController === 1){
+            
+              /*CHECKCOLLISION SPELL 02*/
+              if(this.total.spell02[0]>=0){
+              
+                /*LIFE */
+                this.scoreCharacter-= this.magic02.power;
+                document.getElementById('score-character').style.width = this.scoreCharacter.toString() + '%';
+    
+              }//this.total.spell02>=0
+    
+              this.scoreController = 0;
+            }
+          
+          
+        }
+        
+      }//if(collision)
+     
+    }//if(this.magic)
+
+    /*SPELL 03**************************************/
+    if(this.magic03){
+      if(this.total.spell03[1] === true){
+        /* Magic Position */
+        const magicX = (this.magic03.positionX + this.magic03.startX)*GRID_SIZE;
+        const magicY = (this.magic03.positionY + this.magic03.startY)*GRID_SIZE;
+    
+
+        /*CHECKCOLLISION*/
+        if(
+          /*UP*/
+          Math.round(characterY) <= Math.round(magicY) + 25 &&
+          Math.round(characterX) + 50 >= Math.round(magicX) &&
+
+          /*BOTTOM*/
+          Math.round(characterY) + 50 > Math.round(magicY) 
+          ){
+        
+          if(this.scoreController === 1){
+            
+              /*CHECKCOLLISION SPELL 03*/
+              if(this.total.spell03[0]>=0){
+              
+                /*LIFE */
+                this.scoreCharacter-= this.magic03.power;
+                document.getElementById('score-character').style.width = this.scoreCharacter.toString() + '%';
+    
+              }//this.total.spell03>=0
+    
+              this.scoreController = 0;
+            }
+          
+          
+        }
+        
+      }//if(collision)
+     
+    }//if(this.magic03)
     
     
   }//CheckColision
