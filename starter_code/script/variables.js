558dae407d38de741816a2896527c9e1fa790b3e
Ana Flavia Foppa <anaffoppa@gmail.com>
2020-02-06 20:05:35 +0000

    CSS almost done, but mini bugs need to be resolve

--- starter_code/script/variables.js
+++ starter_code/script/variables.js
@@ -2,8 +2,8 @@ const GRID_SIZE = 50
 
 //Inicial Position Diva
 const INICIAL_DX = 12;
-const INICIAL_DY = 8;
+const INICIAL_DY = 6;
 
 //Inicial Position Characters
 const INICIAL_CX = 4;
-const INICIAL_CY = 8;
+const INICIAL_CY = 6;
